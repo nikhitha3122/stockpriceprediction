de7a587eab20d40a56d347a138abd9ea5c5ae8dd
Tejeswara Murthy palwadi <tejeswarmurthypalwadi@gmail.com>
2022-12-05 23:14:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt 
 import pandas_datareader as data 
+import tensorflow.compat.v2 as tf
+
 
 from keras.models import load_model
 import streamlit as st
